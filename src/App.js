7b93d410396cc49ae9668bd363cd13f2cff1b74a
luferquisa <luferquisa@gmail.com>
2021-10-06 12:00:27 -0500

    fsdfsdf

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 	<p>
-	Editado por Luisa Quiroga Saavedra, en el 2021, Octubre, anio del senior
+	Editado por Luisa Quiroga Saavedra, en el 2021, Octubre, anio del senior. Prueba
 	</p>
         <a
           className="App-link"
